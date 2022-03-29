c0ba9106299836292423dba2ec62fe5f8328223b
ManyuDhyani <manyudhyani@gmail.com>
2022-03-29 10:22:55 +0530

    Main Slider Lower container content added.

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,7 @@
 import 'package:ecom_app/utils/colors.dart';
 import 'package:ecom_app/widgets/big_text.dart';
+import 'package:ecom_app/widgets/icon_and_text_widgets.dart';
+import 'package:ecom_app/widgets/small_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -30,7 +32,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         Container(
           height: 220,
-          margin: EdgeInsets.only(left: 5, right: 5),
+          margin: EdgeInsets.only(left: 10, right: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
@@ -43,11 +45,64 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
+            height: 120,
             margin: EdgeInsets.only(left: 20, right: 20, bottom: 16),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(
+                    text: "Chinese Side",
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      const SizedBox(width: 10),
+                      SmallText(text: "1287"),
+                      const SizedBox(width: 10),
+                      SmallText(text: "comments"),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconAndTextWidget(
+                        icon: Icons.circle_sharp,
+                        text: "Normal",
+                        iconColor: AppColors.iconColor1,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.location_on,
+                        text: "1.7KM",
+                        iconColor: AppColors.mainColor,
+                      ),
+                      IconAndTextWidget(
+                        icon: Icons.access_time_rounded,
+                        text: "32min",
+                        iconColor: AppColors.iconColor2,
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
