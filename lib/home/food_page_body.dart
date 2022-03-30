9015ab8b2399ba08ae6a377a726b696a77a3e494
ManyuDhyani <manyudhyani@gmail.com>
2022-03-30 22:37:13 +0530

    Applied shadow to the lower container.

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -92,9 +92,23 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               height: 120,
               margin: EdgeInsets.only(left: 20, right: 20, bottom: 16),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.white,
-              ),
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFFe8e8e8),
+                      blurRadius: 5.0,
+                      offset: Offset(0, 5),
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-5, 0),
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(5, 0),
+                    )
+                  ]),
               child: Container(
                 padding: EdgeInsets.only(top: 15, left: 15, right: 15),
                 child: Column(
