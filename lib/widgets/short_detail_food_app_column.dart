313c6d9a640c202a60a7844453521c71a22a007a
ManyuDhyani <manyudhyani@gmail.com>
2022-04-04 21:42:43 +0530

    Created Reusable short detail column.

---
+++ lib/widgets/short_detail_food_app_column.dart
@@ -0,0 +1,67 @@
+import 'package:ecom_app/utils/colors.dart';
+import 'package:ecom_app/utils/dimensions.dart';
+import 'package:ecom_app/widgets/big_text.dart';
+import 'package:ecom_app/widgets/icon_and_text_widgets.dart';
+import 'package:ecom_app/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+
+class ShortDetailAppColumn extends StatelessWidget {
+  final String text;
+  const ShortDetailAppColumn({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        BigText(
+          text: text,
+        ),
+        SizedBox(height: Dimensions.height10),
+        // Comments Section
+        Row(
+          children: [
+            Wrap(
+              children: List.generate(
+                5,
+                (index) => Icon(
+                  Icons.star,
+                  color: AppColors.mainColor,
+                  size: 15,
+                ),
+              ),
+            ),
+            SizedBox(width: Dimensions.width10),
+            SmallText(text: "4.5"),
+            SizedBox(width: Dimensions.width20),
+            SmallText(text: "1287"),
+            SizedBox(width: Dimensions.width10),
+            SmallText(text: "comments"),
+          ],
+        ),
+        SizedBox(height: Dimensions.height15),
+        // Time and Distance
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconAndTextWidget(
+              icon: Icons.circle_sharp,
+              text: "Normal",
+              iconColor: AppColors.iconColor1,
+            ),
+            IconAndTextWidget(
+              icon: Icons.location_on,
+              text: "1.7KM",
+              iconColor: AppColors.mainColor,
+            ),
+            IconAndTextWidget(
+              icon: Icons.access_time_rounded,
+              text: "32min",
+              iconColor: AppColors.iconColor2,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
