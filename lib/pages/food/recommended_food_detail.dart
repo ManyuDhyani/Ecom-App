bf1b67b7a2e17e0b20efcc526bb5af684978fcbd
ManyuDhyani <manyudhyani@gmail.com>
2022-04-08 09:44:08 +0530

    Added the Expandable Text Widget for the item description and icons to the page like close and cart.

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -1,6 +1,8 @@
 import 'package:ecom_app/utils/colors.dart';
 import 'package:ecom_app/utils/dimensions.dart';
+import 'package:ecom_app/widgets/app_icon.dart';
 import 'package:ecom_app/widgets/big_text.dart';
+import 'package:ecom_app/widgets/expandable_text_widget.dart';
 import 'package:ecom_app/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 
@@ -10,22 +12,35 @@ class RecommendedFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            toolbarHeight: 70,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.clear),
+                AppIcon(icon: Icons.shopping_cart_outlined)
+              ],
+            ),
             bottom: PreferredSize(
               preferredSize: Size.fromHeight(20),
               child: Container(
-                color: Colors.white,
-                child: Center(
-                  child: BigText(
-                    text: "Chinese Side",
-                    size: Dimensions.font26,
+                  child: Center(
+                    child: BigText(
+                      text: "Chinese Side",
+                      size: Dimensions.font26,
+                    ),
                   ),
-                ),
-                width: double.maxFinite,
-                padding: EdgeInsets.only(top: 5, bottom: 10),
-              ),
+                  width: double.maxFinite,
+                  padding: EdgeInsets.only(top: 5, bottom: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(Dimensions.radius20),
+                        topRight: Radius.circular(Dimensions.radius20),
+                      ))),
             ),
             pinned: true,
             backgroundColor: AppColors.yellowColor,
@@ -39,16 +54,18 @@ class RecommendedFoodDetail extends StatelessWidget {
             ),
           ),
           SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsets.only(
-                  left: Dimensions.width20, right: Dimensions.width20),
-              child: SmallText(
-                text:
-                    "They told her that this was her once chance to show the world what she was made of. She believed them at the time. It was the big stage and she knew the world would be there to see. The only one who had disagreed with this sentiment was her brother. He had told her that you don't show the world what you're made of when they are all watching, you show that in your actions when nobody was looking. It was looking more and more like her brother was correct.",
-                size: Dimensions.font16,
-                color: AppColors.paraColor,
-                height: 1.5,
-              ),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                    left: Dimensions.width20,
+                    right: Dimensions.width20,
+                  ),
+                  child: ExpandableTextWidget(
+                      text:
+                          "They told her that this was her once chance to show the world what she was made of. She believed them at the time. It was the big stage and she knew the world would be there to see. The only one who had disagreed with this sentiment was her brother. He had told her that you don't show the world what you're made of when they are all watching, you show that in your actions when nobody was looking. It was looking more and more like her brother was correct."),
+                )
+              ],
             ),
           ),
           SliverList(
@@ -56,13 +73,13 @@ class RecommendedFoodDetail extends StatelessWidget {
               (BuildContext context, int index) {
                 return Container(
                   color: Colors.white,
-                  height: 10,
+                  height: 430,
                   child: Center(
-                    child: Text('$index', textScaleFactor: 5),
+                    child: Text('$index', textScaleFactor: 0),
                   ),
                 );
               },
-              childCount: 40,
+              childCount: 1,
             ),
           ),
         ],
