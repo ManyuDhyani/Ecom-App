e5085ff301ebd1ba6cadc480c7e277871b2eb586
ManyuDhyani <manyudhyani@gmail.com>
2022-04-11 20:26:06 +0530

    Setup Ecom Backend using Django.

---
+++ EcomBackend/API/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, blank = True, null = True)
+    description = models.TextField(blank = True, null = True)
+    price=models.DecimalField(max_digits=5, decimal_places=2, blank = True, null = True)
+    stars=models.DecimalField(max_digits=5, decimal_places=1,blank = True, null = True)
+    image_url = models.CharField( max_length=150, blank = True, null = True)
+    location = models.CharField(max_length=100, blank = True, null = True)
+    location = models.CharField(max_length=100, blank = True, null = True)
+    location = models.CharField(max_length=100, blank = True, null = True)
+    typeId = models.IntegerField(blank = True, null = True)
+
+    def __str__(self):
+        return self.name
